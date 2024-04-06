b19889709e9587a2cc49f2fd5338db0d5c6f84a9
eugenesvk <eugenesvk@users.noreply.github.com>
2024-04-06 19:05:59 +0700

    lib: add dbg

---
+++ inc_dbg.js
@@ -0,0 +1,29 @@
+﻿// Include file script for Directory Opus, gpsoft.com.au/DScripts/redirect.asp?page=scripts
+function OnInitIncludeFile(D) { // Called by Opus to initialize the include file script
+  D.name   	= "Dbg";
+  D.desc   	= "Debug functions";
+  D.version	= "1.0";
+  D.url    	= "resource.dopus.com/c/buttons-scripts/16";
+  D.shared 	= true; D.min_version = "12.0"; D.copyright = "es";
+}
+
+function p(text) { // alias to print output
+  try        	{ if (Script); DOpus.Output(text     );
+  } catch (e)	{              DOpus.Output(text,true);}
+}
+function err(text) { // alias to print error output
+  try        	{ if (Script); DOpus.Output(text,true);
+  } catch (e)	{              DOpus.Output(text,true);}
+}
+function dbg(text) { // print debug output if script is configured with DebugOutput
+  try {
+    if (Script);
+    if (0 || Script.config.DebugOutput) DOpus.Output(text      );
+  } catch (e) {                         DOpus.Output(text, true);}
+}
+function dbgv(text) { // print verbose debug output if script is configured with DebugVerbose
+  try {
+    if (Script);
+    if (0 || Script.config.DebugVerbose) DOpus.Output(text      );
+  } catch (e) {                          DOpus.Output(text, true);}
+}
